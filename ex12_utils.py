b2b02c8e543046988de4e57ec3e431087423264c
eilonjacob <eilon277@gmail.com>
2021-01-08 22:51:39 +0200

    load_words

--- ex12_utils.py
+++ ex12_utils.py
@@ -1,8 +1,16 @@
 def load_words_dict(file_path):
-    pass
+    """This function gets a filepath of file with records and returns it as a list of records."""
+    with open(file_path) as data_file:
+        records = []
+        for line in data_file:
+            words = line.strip().split()
+            records.append(Record(words[0], words[1:]))
+        return records
+
 
 def is_valid_path(board, path, words):
     pass
+
 
 def find_length_n_words(n, board, words):
     pass
